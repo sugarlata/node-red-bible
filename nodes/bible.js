f21294cd247738867160e60c6b802e97a1dd3f89
Nathan Sgarlata <nathan.sgarlata@gmail.com>
2023-08-04 15:36:49 +1000

    Implements passthrough

--- nodes/bible.js
+++ nodes/bible.js
@@ -1,9 +1,14 @@
 module.exports = function(RED) {
 
     function BibleNode(config) {
-        RED.nodes.createNode(this,config);
 
-        
+        RED.nodes.createNode(this, config);
+        let node = this;
+
+        node.on("input", function(msg) {
+
+            node.send(msg);
+        });
     }
 
     RED.nodes.registerType("bible",BibleNode);
