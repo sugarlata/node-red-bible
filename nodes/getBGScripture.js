c0ca051c79c5b1bb8071477bb0c9e237d2a77f71
Nathan Sgarlata <nathan.sgarlata@gmail.com>
2023-08-10 12:41:43 +1000

    feat(both nodes): adds msg.book, msg.chapter, msg.version to output

--- nodes/getBGScripture.js
+++ nodes/getBGScripture.js
@@ -12,13 +12,17 @@ module.exports = function(RED) {
 
             const book = msg.payload.book || msg.book;
             const chapter = msg.payload.chapter || msg.chapter;
+            const version = constants.DOWNLOADABLE_BIBLE_VERSION;
             const format = msg.payload.format || msg.format || "text";
 
-            const url = utils.bgURLGenerator(book, chapter, version=constants.DOWNLOADABLE_BIBLE_VERSION);
+            const url = utils.bgURLGenerator(book, chapter, version=version);
 
             utils.getScripture(url, format).then(resp => {
 
                 msg.payload = resp;
+                msg.book = book;
+                msg.chapter = chapter;
+                msg.version = version;
                 node.send(msg);
 
             }).catch(e => {
